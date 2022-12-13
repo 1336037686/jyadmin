a91af1736ed1612bf9c0dd587c7011a5b93e4212
LGX_TvT <1336037686@qq.com>
2022-12-13 17:08:24 +0800

    添加项目启动时定时任务重启功能

--- jyadmin-system/src/main/java/com/jyadmin/system/job/config/JobApplicationRunner.java
+++ jyadmin-system/src/main/java/com/jyadmin/system/job/config/JobApplicationRunner.java
@@ -1,11 +1,18 @@
 package com.jyadmin.system.job.config;
 
+import com.jyadmin.system.job.common.QuartzManage;
+import com.jyadmin.system.job.manage.domain.QuartzJob;
+import com.jyadmin.system.job.manage.service.QuartzJobService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 /**
+ * 项目启动时重启定时任务
  * @author LGX_TvT <br>
  * @version 1.0 <br>
  * Create by 2022-11-30 20:27 <br>
@@ -15,10 +22,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class JobApplicationRunner implements ApplicationRunner {
 
+    @Resource
+    private QuartzJobService quartzJobService;
+
+    @Resource
+    private QuartzManage quartzManage;
+
+    /**
+     * 项目启动时重启定时任务
+     */
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        log.info("========================== JobApplicationRunner START ===========================");
-        log.info("========================== JobApplicationRunner END   ===========================");
+    public void run(ApplicationArguments applicationArguments) {
+        log.info("========================== JobApplicationRunner 定时任务重启 START ===========================");
+        List<QuartzJob> jobs = quartzJobService.list();
+        jobs.forEach(quartzManage::addJob);
+        log.info("========================== JobApplicationRunner 定时任务重启 END   ===========================");
     }
+
+
 
 }
