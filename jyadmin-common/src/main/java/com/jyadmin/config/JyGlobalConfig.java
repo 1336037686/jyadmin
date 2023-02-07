15decb4bb16d86db99d799e1d976ede88b44ff00
LGX_TvT <1336037686@qq.com>
2023-02-07 15:15:40 +0800

    feat: 开启mybatis-plus的二级缓存

--- jyadmin-common/src/main/java/com/jyadmin/config/JyGlobalConfig.java
+++ jyadmin-common/src/main/java/com/jyadmin/config/JyGlobalConfig.java
@@ -1,6 +1,7 @@
 package com.jyadmin.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
@@ -12,6 +13,7 @@ import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration
  * Create by 2022-07-04 22:25 <br>
  * @description: JyGlobalConfig <br>
  */
+@EnableCaching // 开启缓存注解
 @EnableConfigurationProperties
 @Import(BeanValidatorPluginsConfiguration.class) // 开启使用JSR303注解 https://doc.xiaominfo.com/knife4j/documentation/jsr303.html
 @Configuration
