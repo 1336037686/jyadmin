55a26d19d907c933805d2264c63d2e5f9cd31146
LGX_TvT <1336037686@qq.com>
2023-05-08 20:02:00 +0800

    feat：添加系统基础配置功能

--- jyadmin-system/src/main/java/com/jyadmin/system/basicset/controller/BasicSettingController.java
+++ jyadmin-system/src/main/java/com/jyadmin/system/basicset/controller/BasicSettingController.java
@@ -1,9 +1,17 @@
 package com.jyadmin.system.basicset.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.jyadmin.domain.Result;
+import com.jyadmin.system.basicset.domain.BasicSetting;
+import com.jyadmin.system.basicset.service.BasicSettingService;
+import com.jyadmin.util.ResultUtil;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 系统基础配置
@@ -14,12 +22,24 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @Api(value = "系统基础配置", tags = "系统：系统基础配置接口")
-@RequestMapping("/api/basicSetting")
+@RequestMapping("/api/basic-setting")
 @RestController
 public class BasicSettingController {
 
+    @Resource
+    private BasicSettingService basicSettingService;
 
+    @ApiOperation(value = "更新系统基础配置", notes = "")
+    @PutMapping("/update")
+    public Result<Object> doUpdate(@RequestBody List<BasicSetting> vo) {
+        return ResultUtil.toResult(basicSettingService.updateBatchById(vo));
+    }
 
-
+    @ApiOperation(value = "列表查询系统基础配置", notes = "")
+    @GetMapping("/list")
+    public Result<List<BasicSetting>> doQueryList() {
+        List<BasicSetting> list = this.basicSettingService.list(new LambdaQueryWrapper<BasicSetting>().orderByAsc(BasicSetting::getId));
+        return Result.ok(list);
+    }
 
 }
